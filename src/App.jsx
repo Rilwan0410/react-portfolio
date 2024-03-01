5905ecde66e0545753392f0ac3e128c1181cbb26
User <user@Users-MacBook-Air.local>
2024-03-01 07:56:50 -0500

    finished main hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 function App() {
   return (
     <>
-      <section className="bg-[linear-gradient(90deg,#0f0c29,#302b63,#24243e)] h-screen w-full grid place-items-center overflow-scroll ">
-        <div className="text-center fixed w-[98%] h-[95%]  rounded-xl items-center text-gray-100 ">
+      <section className="bg-[linear-gradient(90deg,#0f0c29,#302b63,#24243e)] h-screen w-full grid place-items-center fixed overflow-scroll  ">
+        <div className="text-center  rounded-xl items-center text-gray-100  ">
           <Header />
 
-          <div className="content flex  w-full  justify-between mt-[150px] px-[100px]">
+          <div className="content flex  w-full justify-between mt-[150px] h-screen px-[100px]">
+            <div>
+              <div className="left-side top-[9.4rem] sticky flex flex-col items-start">
+                <h1 className="text-[5rem] font-[500]">Rilwan Etti</h1>
+                <h4 className="text-[2rem] font-[300]">Full-Stack Developer</h4>
 
-            <div className="left flex flex-col items-start">
-              <h1 className="text-[6rem] font-[500]">Rilwan Etti</h1>
-              <h4 className="text-[2rem] font-[300]">Full-Stack Developer</h4>
-
-
-              <div className="icons mt-[15rem] flex gap-[25px]">
-             <a href="https://github.com/Rilwan0410" target="_blank"> <i className="fa-brands fa-github text-[2.1rem] text-gray-500 hover:text-white transition duration-500 cursor-pointer"/></a>
-              <i className="fa-brands fa-linkedin text-[2.1rem] text-gray-500 hover:text-white transition duration-500 cursor-pointer"/>
+                <div className="icons mt-[15rem] flex gap-[25px]">
+                  <a href="https://github.com/Rilwan0410" target="_blank">
+                    {" "}
+                    <i className="fa-brands fa-github text-[2.1rem] text-gray-500 hover:text-white transition duration-500 cursor-pointer" />
+                  </a>
+                  <i className="fa-brands fa-linkedin text-[2.1rem] text-gray-500 hover:text-white transition duration-500 cursor-pointer" />
+                </div>
               </div>
-
             </div>
 
-            <div className="right w-1/2 overflow-scroll">
-              <div className="right-content h-[3000px] text-left text-[1.1rem] text-[rgba(255,255,255,0.65)] font-poppins leading-[2rem] mt-[30px] font-[300]">
-                My name is Rilwan Etti and I started programming in 2022 because I wanted to make a switch in careers. I didn't realize then that I had found something that would turn into a passion of mine. I genuinely enjoy building upon my skills as a programmer, and nothing brings me more satisfaction than bridging the gaps in my knowledge as I continue my pursuit to become the best programmer I can be. As of today I have successfully completed a <span class="text-white">full-stack bootcamp</span> for the <span class="text-white">University Of Miami</span>
+            <div className="right-side w-1/2 flex flex-col scroll-pt-[100px]" id='about' >
+              <div className="right-content  text-left text-[1.1rem] text-[rgba(255,255,255,0.65)] font-poppins leading-[2rem]  font-[300]">
+                My name is Rilwan Etti and I started programming in 2022 because
+                I wanted to make a switch in careers. I didn't realize then that
+                I had found something that would turn into a passion of mine. I
+                genuinely enjoy building upon my skills as a programmer, and
+                nothing brings me more satisfaction than bridging the gaps in my
+                knowledge as I continue my pursuit to become the best programmer
+                I can be. As of today I have successfully completed a{" "}
+                <span class="text-white">full-stack bootcamp</span> for the{" "}
+                <span class="text-white">University Of Miami</span>
+              </div>
+
+
+              <div className="stacks">
+              {/* <i class="fa-brands fa-html5"/>
+              <i class="fa-brands fa-css3-alt"/>
+              <i class="fa-brands fa-js"/>
+              <i class="fa-brands fa-node"/> */}
               </div>
             </div>
 
           </div>
-
         </div>
       </section>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Header() {
   return (
-<nav className=' w-[98%] fixed z-10'>
-    <ul className="flex gap-[30px] p-[20px] justify-end  text-[rgba(255,255,255,0.8)] font-montserrat">
-        <a href="">
+<nav className=' ml-[95px] fixed z-10'>
+    <ul className="flex gap-[30px] p-[20px] pt-[40px] justify-end  text-[rgba(255,255,255,0.6)] font-montserrat">
+        <a href="#about">
             <li>About Me</li>
         </a>
         <a href="">
