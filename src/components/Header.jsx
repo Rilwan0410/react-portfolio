244ea8d9daed51fc793f4aa864648b59b46f63b8
User <user@Users-MacBook-Air.local>
2024-03-01 05:45:28 -0500

    added more content to about me section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
 <nav className=' w-[98%] fixed z-10'>
-    <ul className="flex gap-[30px] p-[20px] justify-center  text-[rgba(255,255,255,0.8)] font-montserrat">
+    <ul className="flex gap-[30px] p-[20px] justify-end  text-[rgba(255,255,255,0.8)] font-montserrat">
         <a href="">
             <li>About Me</li>
         </a>
